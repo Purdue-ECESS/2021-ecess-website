25c258e3a886ba3400faf05acce897ab65e5558d
naveen-vivek <vivek@purdue.edu>
2021-10-03 14:31:23 -0700

    Edited commitee page!

--- src/components/Committees/CommitteeCard.js
+++ src/components/Committees/CommitteeCard.js
@@ -1,35 +1,30 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
 
-function CommitteeCard(props) {
-    const {link, name} = props;
+function CommitteeCard2(props) {
     return (
-        <Card className="quote-card-view">
-        <Card.Body>
-            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-                <strong className="yellow">
-                    {name}
-                </strong>
-            </h1>
-            <h1 style={{ fontSize: "1.2em", paddingBottom: "10px" }}>
-                <strong className="yellow">Head: {props.head}</strong>
-            </h1>
-            <h1 style={{ fontSize: "1.2em", paddingBottom: "10px" }}>
-                <strong className="yellow">Email: <a href="mailto:{props.email}"> {props.email}</a></strong>
-            </h1>
-            {link ?
-                <Button variant="primary" href={link} target="_blank">
-                    {"Website"}
-                </Button> : <></>
-            }
-            <blockquote className="blockquote mb-0" style={{marginTop: 10}}>
-                <p style={{ textAlign: "center" }}>
-                {props.description}
-                </p>
-            </blockquote>
-        </Card.Body>
-    </Card>
+        <Card className="project-card-view">
+            <Card.Body>
+                
+                <Card.Title>
+                    <strong className="yellow">
+                        {props.name}
+                    </strong>
+                </Card.Title>
+
+                <Card.Text style={{ textAlign: "center" }}>
+                    <strong className="yellow">
+                        Head: {props.head} <br />
+                        Contact: <a href="mailto:{props.contact}"> {props.email}</a>
+                    </strong>
+                </Card.Text>
+
+                <Card.Text style={{ textAlign: "center" }}>
+                    {props.description}
+                </Card.Text>
+
+            </Card.Body>
+        </Card>
     );
 }
-export default CommitteeCard;
+export default CommitteeCard2;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,6 +5,14 @@ import Card from "react-bootstrap/Card";
 import Particle from "../Particle";
 import Type from "./Type";
 
+import {
+  AiFillGithub,
+  AiFillInstagram,
+} from "react-icons/ai";
+
+import { SiDiscord } from "react-icons/si";
+
+
 function Home() {
   return (
       <section>
@@ -26,9 +34,27 @@ function Home() {
         <Card className="quote-card-view">
           <Card.Body>
             <div style={{ padding: 1, textAlign: "center" }}>
-            Email: <a class="nobubble" href="mailto:vivek@purdue.edu">ecess@purdue.edu</a> <br/>
+            Email: <a class="nobubble" href="mailto:ecess@purdue.edu">ecess@purdue.edu</a> <br/>
             Address: EE 232, 475 Northwestern Avenue, West Lafayette IN - 47907
           </div>
+          </Card.Body>
+        </Card>
+
+        <Card className="quote-card-view">
+        <Card.Body>
+        <div style={{ padding: 1, textAlign: "center" }}>
+          <a href="https://github.com/ecesspurdue" style={{ color: "#cfb991" }}>
+            <AiFillGithub size={50} />
+          </a>
+          {" "}
+          <a href="https://www.instagram.com/purdue_ecess" style={{ color: "#cfb991" }}>
+            <AiFillInstagram size={50} />
+          </a>
+          {" "}
+          <a href="https://discord.gg/g2frztsC8W" style={{ color: "#cfb991" }}>
+            <SiDiscord size={50} />
+          </a>
+        </div>
           </Card.Body>
         </Card>
 
